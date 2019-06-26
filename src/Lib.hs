8e86f1eaed1bca797e39e6613bff5f2cdc4a33ee
Mark Keaton <mkeaton@gmail.com>
2019-06-26 10:43:07 -0500

    Basic proof of concept to fetch PRs.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,16 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
   ( someFunc
-  , Notification (..)
+  , Notification(..)
   , getIconLocation
+  , getAuth
+  , testPR
   )
 where
 
 -- Base/Prelude
-import Text.Printf (printf)
+import           Control.Applicative            ( (<*>) )
+import qualified Data.ByteString.Char8         as B
+import System.Environment (lookupEnv)
+import           Text.Printf                    ( printf )
 
 -- 3rd Party
-import System.Directory (getAppUserDataDirectory)
-import System.Process (ProcessHandle, spawnCommand)
+import           GitHub.Auth                    ( Auth(OAuth) )
+import qualified GitHub.Endpoints.PullRequests as PR
+import           System.Directory               ( getAppUserDataDirectory )
+import           System.Process                 ( ProcessHandle
+                                                , spawnCommand
+                                                )
 
 data Notification = Notification { title :: String
                                  , message :: String }
@@ -24,5 +35,18 @@ someFunc :: Notification -> IconLocation -> IO ProcessHandle
 someFunc n iconLocation = spawnCommand (formatCommand n iconLocation)
 
 formatCommand :: Notification -> IconLocation -> String
-formatCommand n iconLocation = printf "notify-send -i %s \"%s\" \"%s\"" iconLocation (title n) (message n)
+formatCommand n iconLocation =
+  printf "notify-send -i %s \"%s\" \"%s\"" iconLocation (title n) (message n)
 
+getAuth = do
+  -- user <- lookupEnv "GITHUB_USERNAME"
+  pass <- lookupEnv "GITHUB_TOKEN"
+  -- let userB = fmap B.pack user
+  let passB = fmap B.pack pass
+  let auth = fmap OAuth passB
+  return auth
+
+testPR = do
+  auth <- getAuth
+  prs <- PR.pullRequestsFor' auth "Tesorio" "Dashboard"
+  print prs
