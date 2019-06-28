89760069923d8f3bd43a79b659321a8c9f3e9a20
Mark Keaton <mkeaton@gmail.com>
2019-06-28 17:02:20 -0500

    Start adding filtering and time diff.

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,11 +12,21 @@ where
 -- Base/Prelude
 import           Control.Applicative            ( (<*>) )
 import qualified Data.ByteString.Char8         as B
-import System.Environment (lookupEnv)
+import           System.Environment             ( lookupEnv )
 import           Text.Printf                    ( printf )
 
 -- 3rd Party
+import           Data.Time.Clock                ( NominalDiffTime
+                                                , UTCTime
+                                                , diffUTCTime
+                                                , getCurrentTime
+                                                )
+import qualified Data.Vector                   as V
 import           GitHub.Auth                    ( Auth(OAuth) )
+import           GitHub.Data.Definitions        ( Error
+                                                , SimpleUser
+                                                )
+import           GitHub.Data.PullRequests       ( SimplePullRequest )
 import qualified GitHub.Endpoints.PullRequests as PR
 import           System.Directory               ( getAppUserDataDirectory )
 import           System.Process                 ( ProcessHandle
@@ -27,6 +37,8 @@ data Notification = Notification { title :: String
                                  , message :: String }
 type IconLocation = String
 
+-- simplePullRequestRequestedReviewers
+userId = "3433130"  -- mark-keaton
 
 getIconLocation :: FilePath -> String
 getIconLocation appUserDataDir = appUserDataDir ++ "/github.png"
@@ -38,15 +50,25 @@ formatCommand :: Notification -> IconLocation -> String
 formatCommand n iconLocation =
   printf "notify-send -i %s \"%s\" \"%s\"" iconLocation (title n) (message n)
 
+getAuth :: IO (Maybe Auth)
 getAuth = do
-  -- user <- lookupEnv "GITHUB_USERNAME"
   pass <- lookupEnv "GITHUB_TOKEN"
-  -- let userB = fmap B.pack user
   let passB = fmap B.pack pass
-  let auth = fmap OAuth passB
+  let auth  = fmap OAuth passB
   return auth
 
 testPR = do
   auth <- getAuth
-  prs <- PR.pullRequestsFor' auth "Tesorio" "Dashboard"
-  print prs
+  return $ PR.pullRequestsFor' auth "Tesorio" "Dashboard"
+
+isAssigned :: SimpleUser -> SimplePullRequest -> Bool
+isAssigned user pr =
+  V.any (user ==) (PR.simplePullRequestRequestedReviewers pr)
+
+extractAssignments
+  :: SimpleUser -> V.Vector SimplePullRequest -> V.Vector SimplePullRequest
+extractAssignments user = V.filter (isAssigned user)
+
+isRecentPR :: NominalDiffTime -> UTCTime -> SimplePullRequest -> Bool
+isRecentPR expectedDelta now pr = delta < expectedDelta
+  where delta = diffUTCTime now (PR.simplePullRequestUpdatedAt pr)
